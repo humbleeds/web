d4c8eae8a4ccd4cc4f7eb4b9acf678df45cc25c3
Aliaksandra <humble.sasha.da@gmail.com>
2021-05-11 18:57:05 +0300

    Init

---
+++ src/components/memberships.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../App";
+
+function Memberships () {
+
+    return (
+        <div>
+    
+            <h3>Hello from memberships page!</h3>
+        </div>
+    )
+
+}
+
+export default Memberships; 
---
+++ src/components/stats.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../App";
+
+function Statistics () {
+
+    return (
+        <div>
+            <h3>Hello from statistics page!</h3>
+        </div>
+    )
+
+}
+
+export default Statistics; 
